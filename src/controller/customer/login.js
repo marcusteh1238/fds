ee430cba5c6967c9864e9c06dd5b19fdce16e3d4
marcusteh1238 <marcusteh1238@gmail.com>
2020-03-29 16:54:58 +0800

    Remove password param for customers

--- src/controller/customer/login.js
+++ src/controller/customer/login.js
@@ -2,9 +2,9 @@ const db = require('../../db')
 // Params: username, password
 async function loginImpl(params) {
 
-    const { username, password } = params;
+    const { username } = params;
     
-    const response = await db.query("SELECT * FROM customers c WHERE c.username = $1 AND c.password = $2", [username, password])
+    const response = await db.query("SELECT * FROM customers c WHERE c.username = $1", [username])
     const rows = response.rows;
     if (rows.length < 1) {
         return {
