be0ced607f19d861187ba90d2c68bc210211166c
longluqian <e0323614@u.nus.edu>
2020-03-29 18:48:37 +0800

    Update sqlScript

--- src/sql/psqlscript.sql
+++ src/sql/psqlscript.sql
@@ -46,6 +46,7 @@ CREATE TABLE employmentType{
 employmentTypeId INTEGER NOT NULL,
 employmentType VARCHAR(10) NOT NULL,
 baseSalary NUMERIC NOT NULL,
+perOrderSalary NUMERIC NOT NULL,
 PRIMARY KEY(employmentTypeId)
 
 };
@@ -126,7 +127,7 @@ PRIMARY KEY(oId,fId)
 
 
 CREATE TABLE WorkInterval(
-riderId Integer NOT NULL references DeliveryRiders,
+rId Integer NOT NULL references DeliveryRiders,
 intervalId  Integer NOT NULL,
 date Date NOT NULL,
 start_time TIME NOT NULL,
@@ -200,12 +201,9 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER add_point
 AFTER INSERT ON Orders
 EXECUTE PROCEDURE add_reward_point();
-
 --dummy data
 INSERT INTO Customers(cId,username,rewardPoints,joinDate,registeredCreditCard) VALUES(1,'Sade',0,'11/01/2019','5377830405202395'),(2,'Laura',0,'08/22/2020','5499023976889336'),(3,'Bruce',0,'01/02/2021','5117954070692188'),(4,'Kylynn',0,'03/12/2021','5448082263759919'),(5,'Tad',0,'04/03/2019','5382940467214320'),(6,'Nigel',0,'05/07/2019','5225096766781138'),(7,'Nita',0,'07/23/2020','5192549986365057'),(8,'Keelie',0,'09/01/2019','5527789691763594'),(9,'Deirdre',0,'05/26/2020','5109510697360730'),(10,'Celeste',0,'01/12/2020','5130375140838275');
 INSERT INTO Restaurants(RId,RName,minOrderPrice,locationArea,RAddress) VALUES(1,'Maggy',16,'21875','37248'),(2,'Chadwick',12,'624770','10468'),(3,'Beau',15,'48565','25083'),(4,'Hasad',17,'920639','211930'),(5,'Kieran',12,'24503','51963'),(6,'Xander',18,'027649','6082'),(7,'Yeo',16,'63669','P2J 5Y9'),(8,'Leila',16,'GY3L 8JL','7123'),(9,'Abdul',19,'Z0165','07086'),(10,'Fatima',17,'11964','41182');
 INSERT INTO RestaurantsStaff(username,password) VALUES('Adara','9808'),('Kasper','1801'),('Philip','3267'),('Alison','3087'),('Tall','6471'),('Deacon','5480'),('Ali','8239'),('Beck','1964'),('Xenos','9717'),('Chris','7723');
 INSERT INTO FDSManagers(username,password) VALUES('Tiger','4974'),('Dahlia','9056'),('Lara','9681'),('Ali','3152'),('Iona','3652'),('Clayton','1429'),('Fulton','1588'),('Blair','4301'),('Molly','2595'),('Davis','6345');
 INSERT INTO FoodItemCategories(categoryId,name) VALUES(1,'Chicken'),(2,'Fish'),(3,'Rice'),(4,'Noodle'),(5,'Drinks');
-
-
