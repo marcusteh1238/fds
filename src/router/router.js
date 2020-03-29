c5d826da6b2c8cb92103c566c3446d0faaeee160
marcusteh1238 <marcusteh1238@gmail.com>
2020-03-29 16:45:22 +0800

    Add restaurant staff and fds manager routes to router

--- src/router/router.js
+++ src/router/router.js
@@ -3,8 +3,12 @@ const router = express.Router({ mergeParams: true });
 
 // Require endpoints here
 const customerRoute = require('./customer');
+const restaurantstaffRoute = require('./restaurantstaff');
+const fdsmanagerRoute = require('./fdsmanager');
 
 // Endpoints here
 router.use('/customer/', customerRoute);
+router.use('/restaurantstaff/', restaurantstaffRoute);
+router.use('/fdsmanager/', fdsmanagerRoute);
 
 module.exports = router;
