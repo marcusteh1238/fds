5b119ed3e2ce0ca02b95e487d65cc86ae85c89ad
marcusteh1238 <marcusteh1238@gmail.com>
2020-03-29 16:40:05 +0800

    Add actual login implementation

--- src/controller/customer/login.js
+++ src/controller/customer/login.js
@@ -4,20 +4,19 @@ async function loginImpl(params) {
 
     const { username, password } = params;
     
-    const dbPass = await getCustomerFromUsername(username);
-    const isMatchingPassword = dbPass === password;
-    const res = await db.query("SELECT 1 FROM customers c WHERE c.username = $1 AND c.password = $2", [username, password], (err, res)=>{console.log(res)})
-    console.log(res)
+    const response = await db.query("SELECT * FROM customers c WHERE c.username = $1 AND c.password = $2", [username, password])
+    const rows = response.rows;
+    if (rows.length < 1) {
+        return {
+            isLoginSuccess: false
+        }
+    }
+    const user = rows[0]
     // return endpoint results
     return {
-        username: username,
-        correctPass: isMatchingPassword
+        isLoginSuccess: true,
+        user
     };
-}
-
-async function getCustomerFromUsername(username) {
-    // return db.query('BLAH')
-    return 'password'
 }
 
 module.exports.get = loginImpl;
