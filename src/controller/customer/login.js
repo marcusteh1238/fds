69d68c718b22bdbe1cba569d0c049ffc78e7cc64
marcusteh1238 <marcusteh1238@gmail.com>
2020-03-29 14:03:46 +0800

    Add login endpoint

--- src/controller/customer/login.js
+++ src/controller/customer/login.js
@@ -1,8 +1,25 @@
+const { Client } = require('pg')
+const saltRounds = 5;
+
+const client = new Client()
+// Params: username, password
 async function loginImpl(params) {
+
+    const { username, password } = params;
+    
+    const dbPass = await getPassword(username);
+    const isMatchingPassword = dbPass === password;
+
     // return endpoint results
     return {
-        hello: true
+        username: username,
+        correctPass: isMatchingPassword
     };
+}
+
+async function getPassword(username) {
+    // return client.query('BLAH')
+    return 'password'
 }
 
 module.exports.get = loginImpl;
