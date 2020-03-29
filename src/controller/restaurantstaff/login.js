fa54d996cc9fc989e121ccf8701a48f7e3dcbd9e
marcusteh1238 <marcusteh1238@gmail.com>
2020-03-29 16:45:52 +0800

    Add login for fds manager and restaurant staff

--- src/controller/restaurantstaff/login.js
+++ src/controller/restaurantstaff/login.js
@@ -1,23 +1,22 @@
 const db = require('../../db')
-
 // Params: username, password
 async function loginImpl(params) {
 
     const { username, password } = params;
     
-    const dbPass = await getCustomerFromUsername(username);
-    const isMatchingPassword = dbPass === password;
-
+    const response = await db.query("SELECT * FROM restaurantsstaff c WHERE c.username = $1 AND c.password = $2", [username, password])
+    const rows = response.rows;
+    if (rows.length < 1) {
+        return {
+            isLoginSuccess: false
+        }
+    }
+    const user = rows[0]
     // return endpoint results
     return {
-        username: username,
-        correctPass: isMatchingPassword
+        isLoginSuccess: true,
+        user
     };
-}
-
-async function getCustomerFromUsername(username) {
-    // return db.query('BLAH')
-    return 'password'
 }
 
 module.exports.get = loginImpl;
