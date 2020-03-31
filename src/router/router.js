9f8ed84ea061835c789ef4da2d257e8a63a9ab88
marcusteh1238 <marcusteh1238@gmail.com>
2020-03-31 17:03:09 +0800

    Add rider route

--- src/router/router.js
+++ src/router/router.js
@@ -6,11 +6,13 @@ const customerRoute = require('./customer');
 const restaurantstaffRoute = require('./restaurantstaff');
 const fdsmanagerRoute = require('./fdsmanager');
 const restaurantRoute = require('./restaurant');
+const riderRoute = require('./rider');
 
 // Endpoints here
 router.use('/customer/', customerRoute);
 router.use('/restaurantstaff/', restaurantstaffRoute);
 router.use('/fdsmanager/', fdsmanagerRoute);
 router.use('/restaurant/', restaurantRoute);
+router.use('/rider/', riderRoute);
 
 module.exports = router;
