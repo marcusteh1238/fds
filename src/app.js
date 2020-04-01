bbb0ee4491074f6eeb18ac985f2580d7932d92e6
marcusteh1238 <marcusteh1238@gmail.com>
2020-04-01 14:55:57 +0800

    Enable all CORS requests

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ require('dotenv').config({path: '.env'})
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT;
 const router = require('./router/router');
 
+app.use(cors());
 app.use('/fds', router);
 
 app.listen(port, () => console.log(`FDS Express Backend listening on port ${port}!`));
