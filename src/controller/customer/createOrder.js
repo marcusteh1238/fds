3027bf761d7661fc5f7899e86c4fed3076b60823
marcusteh1238 <marcusteh1238@gmail.com>
2020-04-23 17:33:56 +0800

    Add createOrder endpoint

---
+++ src/controller/customer/createOrder.js
@@ -0,0 +1,16 @@
+const { add_order } = require('../../sql/sqlScript')
+
+/**
+ * Creates a brand new order
+ * @param {JSON} body 
+ */
+async function createOrderImpl({cid, address, promoId = ""}) {
+    const { rowCount } = await db.query(get_customer_by_name, [username]);
+    if (rowCount === 0) {
+        throw Error("Customer does not exists!");
+    }
+    const { rows, rowCount } = await db.query(add_order, [cid, address, promoId]);
+    return {}; // success
+}
+
+module.exports.post = createOrderImpl;
--- src/router/customer.js
+++ src/router/customer.js
@@ -4,6 +4,7 @@ const controllerPath = "../controller/customer";
 const CustomerLoginController = require(`${controllerPath}/login`);
 const GetCustomerController = require(`${controllerPath}/getCustomer`);
 const AddNewCustomerController = require(`${controllerPath}/addNewCustomer`);
+const CreateOrderController = require(`${controllerPath}/createOrder`);
 
 router.get("/login/:username", async(req, res) => {
     try {
@@ -36,6 +37,20 @@ router.get("/getCustomer/:cid", async(req, res) => {
 router.post("/addNewCustomer", async(req, res) => {
     try {
         const response = await AddNewCustomerController.post(req.body);
+        res.status(200).send(response);
+    } catch (error) {
+        console.error(error);
+        const errorCode = error.statusCode ? error.statusCode : 400;
+        res.status(errorCode).send({
+            msg: error.message,
+            error: error
+        })
+    }
+});
+
+router.post("/createOrder", async(req, res) => {
+    try {
+        const response = await CreateOrderController.put(req.body);
         res.status(200).send(response);
     } catch (error) {
         console.error(error);
