76c0cb996d23393aabb41114d1221851793ca6ae
longluqian <e0323614@u.nus.edu>
2020-05-06 11:34:23 +0800

    fixed psqlscript.sql

--- src/sql/psqlscript.sql
+++ src/sql/psqlscript.sql
@@ -6,10 +6,11 @@ DROP TABLE IF EXISTS PromoForCustomer CASCADE;
 DROP TABLE IF EXISTS Orders CASCADE;
 DROP TABLE IF EXISTS OrderDetails CASCADE;
 DROP TABLE IF EXISTS Customers CASCADE;
-DROP TABLE IF EXISTS Restaurants_Staff CASCADE;
-DROP TABLE IF EXISTS FDS_Managers CASCADE;
+DROP TABLE IF EXISTS RestaurantsStaff CASCADE;
+DROP TABLE IF EXISTS fdsmanagers CASCADE;
 DROP TABLE IF EXISTS DeliveryRiders CASCADE;
 DROP TABLE IF EXISTS WorkInterval CASCADE;
+DROP TABLE IF EXISTS employmenttype CASCADE;
 
 
 CREATE TABLE Customers(
@@ -42,14 +43,14 @@ RAddress VARCHAR(100) NOT NULL,
 PRIMARY KEY (RId)
 );
 
-CREATE TABLE employmentType{
+CREATE TABLE employmentType(
 employmentTypeId INTEGER NOT NULL,
 employmentType VARCHAR(10) NOT NULL,
 baseSalary NUMERIC NOT NULL,
 perOrderSalary NUMERIC NOT NULL,
 PRIMARY KEY(employmentTypeId)
 
-};
+);
 
 CREATE TABLE DeliveryRiders(
 rId INTEGER NOT NULL,
@@ -102,7 +103,7 @@ PRIMARY KEY(pId,cId)
 CREATE TABLE Orders(
 oId INTEGER NOT NULL,
 cId INTEGER REFERENCES Customers,
-riderId INTEGER REFERENCES DeliveryRiders,
+riderId INTEGER REFERENCES DeliveryRiders (rId),
 address VARCHAR(20) NOT NULL,
 rating INTEGER DEFAULT 5,
 reviews VARCHAR(50),
@@ -128,13 +129,13 @@ PRIMARY KEY(oId,fId)
 
 CREATE TABLE WorkInterval(
 rId Integer NOT NULL references DeliveryRiders,
-intervalId  Integer NOT NULL,w
+intervalId  Integer NOT NULL,
 date Date NOT NULL,
 start_time TIME NOT NULL,
 end_time TIME NOT NULL,
 weekNum INTEGER NOT NULL,
 mwsId INTEGER,
-PRIMARY KEY(riderId, intervalId)    
+PRIMARY KEY(rId, intervalId)    
 );
 
 
@@ -144,13 +145,13 @@ PRIMARY KEY(riderId, intervalId)
 CREATE OR REPLACE FUNCTION check_food_limit()
 RETURNS TRIGGER AS $$
 	DECLARE 
-		foodName text
+		foodName text;
     BEGIN
 		SELECT FoodItems.foodName into foodName FROM FoodItems WHERE FoodItems.foodItemId = NEW.fId and FoodItems.itemAvailability='F';
         IF foodName is not null THEN
-			RAISE exception 'The Food Item with id % name % is not available for ordering because it has reached its daily maximum', NEW.fId, 
+			RAISE exception 'The Food Item with id % name % is not available for ordering because it has reached its daily maximum', NEW.fId, NEW.foodName;
     END IF;
-    RETURN NEW
+    RETURN NEW;
     END;
     
 $$ LANGUAGE plpgsql;
@@ -159,29 +160,30 @@ DROP TRIGGER IF EXISTS check_new_food_limit ON OrderDetails;
 CREATE TRIGGER check_new_food_limit
 BEFORE UPDATE OF quantity or INSERT ON OrderDetails
 FOR EACH ROW 
-EXECUTE PROCEDURE check_food_limit();
+EXECUTE FUNCTION check_food_limit();
 
 -- TRIGGER 1.2: update the status of foodItems into 'F' if it reaches daily limit
-CREATE OR REPLACE FUNCTION update_food_status(){
-RETURN TRIGGER AS $$ 
+CREATE OR REPLACE FUNCTION update_food_status()
+RETURNS TRIGGER AS $$ 
 	DECLARE totalNumberSold NUMERIC;
     BEGIN 
-		SELECT sum(fi.quantity) INTO count FROM FoodItems fi, Orders o, OrderDetails od
+		SELECT sum(fi.quantity) INTO totalNumberSold FROM FoodItems fi, Orders o, OrderDetails od
 		WHERE NEW.date(timeOrderPlaced) = o.date(timeOrderPlaced) AND
 		o.oid = od.oid AND
 		od.fid = fi.fid AND
 		od.fid = NEW.fid; 
-		IF count = fi.daily_limit AND itemAvailability <> 'F' THEN
+		IF totalNumberSold = fi.daily_limit AND itemAvailability <> 'F' THEN
 		UPDATE FoodItems SET itemAvailability = 'F' WHERE fid = NEW.fid;
 		END IF;
 		RETURN NULL;
 	END;
-$$ LANGUAGE plpgsql
+$$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS update_food_status ON OrderDetails;
+DROP TRIGGER IF EXISTS updateLimitTrigger ON OrderDetails;
 CREATE TRIGGER updateLimitTrigger 
 AFTER INSERT OR UPDATE ON OrderDetails
-FOR EACH ROW EXECUTE update_food_status();
+FOR EACH ROW 
+EXECUTE FUNCTION update_food_status();
     
     
 
@@ -205,7 +207,7 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER check_min_order
 BEFORE INSERT OR UPDATE ON Orders
-EXECUTE PROCEDURE check_minimum();
+EXECUTE FUNCTION check_minimum();
 
 
 -- TRIGGER 3: add reward point to customer after order has been completed
@@ -224,12 +226,12 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER add_point
 AFTER INSERT ON Orders
-EXECUTE PROCEDURE add_reward_point();
+EXECUTE FUNCTION add_reward_point();
 
 
 --dummy data
 INSERT INTO Customers(cId,username,rewardPoints,joinDate,registeredCreditCard) VALUES(1,'Sade',0,'11/01/2019','5377830405202395'),(2,'Laura',0,'08/22/2020','5499023976889336'),(3,'Bruce',0,'01/02/2021','5117954070692188'),(4,'Kylynn',0,'03/12/2021','5448082263759919'),(5,'Tad',0,'04/03/2019','5382940467214320'),(6,'Nigel',0,'05/07/2019','5225096766781138'),(7,'Nita',0,'07/23/2020','5192549986365057'),(8,'Keelie',0,'09/01/2019','5527789691763594'),(9,'Deirdre',0,'05/26/2020','5109510697360730'),(10,'Celeste',0,'01/12/2020','5130375140838275');
 INSERT INTO Restaurants(RId,RName,minOrderPrice,locationArea,RAddress) VALUES(1,'Maggy',16,'21875','37248'),(2,'Chadwick',12,'624770','10468'),(3,'Beau',15,'48565','25083'),(4,'Hasad',17,'920639','211930'),(5,'Kieran',12,'24503','51963'),(6,'Xander',18,'027649','6082'),(7,'Yeo',16,'63669','P2J 5Y9'),(8,'Leila',16,'GY3L 8JL','7123'),(9,'Abdul',19,'Z0165','07086'),(10,'Fatima',17,'11964','41182');
-INSERT INTO RestaurantsStaff(username,password) VALUES('Adara','9808'),('Kasper','1801'),('Philip','3267'),('Alison','3087'),('Tall','6471'),('Deacon','5480'),('Ali','8239'),('Beck','1964'),('Xenos','9717'),('Chris','7723');
-INSERT INTO FDSManagers(username,password) VALUES('Tiger','4974'),('Dahlia','9056'),('Lara','9681'),('Ali','3152'),('Iona','3652'),('Clayton','1429'),('Fulton','1588'),('Blair','4301'),('Molly','2595'),('Davis','6345');
+-- INSERT INTO RestaurantsStaff(username,password) VALUES('Adara','9808'),('Kasper','1801'),('Philip','3267'),('Alison','3087'),('Tall','6471'),('Deacon','5480'),('Ali','8239'),('Beck','1964'),('Xenos','9717'),('Chris','7723');
+-- INSERT INTO FDSManagers(username,password) VALUES('Tiger','4974'),('Dahlia','9056'),('Lara','9681'),('Ali','3152'),('Iona','3652'),('Clayton','1429'),('Fulton','1588'),('Blair','4301'),('Molly','2595'),('Davis','6345');
 INSERT INTO FoodItemCategories(categoryId,name) VALUES(1,'Chicken'),(2,'Fish'),(3,'Rice'),(4,'Noodle'),(5,'Drinks');
