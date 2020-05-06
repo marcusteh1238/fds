9bc4b31b051db42a5ebc569a693307f4852eaa74
marcusteh1238 <marcusteh1238@gmail.com>
2020-05-07 04:53:01 +0800

    Fix addNewCustomer query

--- src/controller/customer/addNewCustomer.js
+++ src/controller/customer/addNewCustomer.js
@@ -1,15 +1,18 @@
+const db = require('../../db')
 const { add_customer, get_customer_by_name } = require('../../sql/sqlScript')
 
 /**
  * 
  * @param {JSON} body 
  */
-async function addNewCustomerImpl({username, fullName, rewardPoints, registeredCreditCard}) {
+// async function addNewCustomerImpl({username, password, rewardPoints, registeredCreditCard}) {
+    async function addNewCustomerImpl(body) {
+        console.log('body: ', body);
     const { rows, rowCount } = await db.query(get_customer_by_name, [username]);
     if (rowCount > 0) {
         throw Error("Customer already exists!");
     }
-    const response = await db.query(add_customer, [username, fullName, rewardPoints, registeredCreditCard])
+    const response = await db.query(add_customer, [username, password, rewardPoints, registeredCreditCard])
     return db.query(get_customer_by_name, [username]);
 }
 
