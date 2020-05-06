c0229b6b806f57b3f28f34beebda955752af9231
marcusteh1238 <marcusteh1238@gmail.com>
2020-05-07 04:31:33 +0800

    Add endpoints to restaurant router

--- src/router/restaurant.js
+++ src/router/restaurant.js
@@ -1,6 +1,7 @@
 const router = require('express').Router({ mergeParams: true });
 
 const controllerPath = "../controller";
+const GetRestaurantController = require(`${controllerPath}/restaurant/getRestaurant`)
 const RestaurantListController = require(`${controllerPath}/restaurant/list`);
 const ViewMonthlyOrderController = require(`${controllerPath}/restaurant/viewMonthlyOrder`);
 const ViewTotalCostOfOrderController = require(`${controllerPath}/restaurant/viewTotalCostOfOrder`);
@@ -8,6 +9,21 @@ const ViewTopFiveFoodItemsController = require(`${controllerPath}/restaurant/vie
 const GetPromoSummaryController = require(`${controllerPath}/restaurant/getPromoSummary`);
 const GetPromoController = require(`${controllerPath}/restaurant/getPromo`);
 const UpdateDailyOrderLimitController = require(`${controllerPath}/restaurant/updateDailyOrderLimit`);
+const UpdateMinOrderAmountController = require(`${controllerPath}/restaurant/updateMinOrderAmt`);
+
+router.get("/getRestaurant/:rid", async(req, res) => {
+    try {
+        const response = await GetRestaurantController.get(req.params);
+        res.status(200).send(response);
+    } catch (error) {
+        console.error(error);
+        const errorCode = error.statusCode ? error.statusCode : 400;
+        res.status(errorCode).send({
+            msg: error.message,
+            error: error
+        })
+    }
+});
 
 router.get("/list", async(req, res) => {
     try {
@@ -96,6 +112,21 @@ router.get("/getPromo/:pid", async(req, res) => {
 router.post("/updateDailyOrderLimit/:rid/:fooditemid/:newLimit", async(req, res) => {
     try {
         const response = await UpdateDailyOrderLimitController.post(req.params);
+        res.status(200).send(response);
+    } catch (error) {
+        console.log(error);
+        const errorCode = error.statusCode ? error.statusCode: 400;
+        res.status(errorCode).send({
+            msg: error.message,
+            error: error
+        })
+    }
+});
+
+
+router.post("/updateMinOrderAmt/:rid/:newMinAmt", async(req, res) => {
+    try {
+        const response = await UpdateMinOrderAmountController.post(req.params);
         res.status(200).send(response);
     } catch (error) {
         console.log(error);
