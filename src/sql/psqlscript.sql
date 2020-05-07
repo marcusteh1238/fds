38b840aafaa19cf93c8085d8dbc31216156b5cda
marcusteh1238 <marcusteh1238@gmail.com>
2020-05-07 17:15:18 +0800

    Remove quantity from FoodItems table, add some dummy data

--- src/sql/psqlscript.sql
+++ src/sql/psqlscript.sql
@@ -72,7 +72,6 @@ CREATE TABLE FoodItems (
 foodItemId SERIAL NOT NULL UNIQUE,
 foodName VARCHAR(20) NOT NULL,
 price NUMERIC NOT NULL,
-quantity INTEGER NOT NULL DEFAULT 0,
 daily_limit INTEGER NOT NULL,
 itemAvailability VARCHAR (1) check (itemAvailability in ('T','F')),
 rId INTEGER REFERENCES Restaurants,
@@ -239,8 +238,9 @@ EXECUTE FUNCTION add_reward_point();
 
 
 --dummy data
--- INSERT INTO Customers(cId,username,rewardPoints,joinDate,registeredCreditCard) VALUES(1,'Sade',0,'11/01/2019','5377830405202395'),(2,'Laura',0,'08/22/2020','5499023976889336'),(3,'Bruce',0,'01/02/2021','5117954070692188'),(4,'Kylynn',0,'03/12/2021','5448082263759919'),(5,'Tad',0,'04/03/2019','5382940467214320'),(6,'Nigel',0,'05/07/2019','5225096766781138'),(7,'Nita',0,'07/23/2020','5192549986365057'),(8,'Keelie',0,'09/01/2019','5527789691763594'),(9,'Deirdre',0,'05/26/2020','5109510697360730'),(10,'Celeste',0,'01/12/2020','5130375140838275');
+INSERT INTO Customers(cId,username,password,rewardPoints,joinDate,registeredCreditCard) VALUES (1, "Mark", "owowats", 1, "02-02-2020", "1234123412341234");
 INSERT INTO Restaurants(RId,RName,minOrderPrice,locationArea,RAddress) VALUES(1,'Maggy',16,'21875','37248'),(2,'Chadwick',12,'624770','10468'),(3,'Beau',15,'48565','25083'),(4,'Hasad',17,'920639','211930'),(5,'Kieran',12,'24503','51963'),(6,'Xander',18,'027649','6082'),(7,'Yeo',16,'63669','P2J 5Y9'),(8,'Leila',16,'GY3L 8JL','7123'),(9,'Abdul',19,'Z0165','07086'),(10,'Fatima',17,'11964','41182');
 -- INSERT INTO RestaurantsStaff(username,password) VALUES('Adara','9808'),('Kasper','1801'),('Philip','3267'),('Alison','3087'),('Tall','6471'),('Deacon','5480'),('Ali','8239'),('Beck','1964'),('Xenos','9717'),('Chris','7723');
 -- INSERT INTO FDSManagers(username,password) VALUES('Tiger','4974'),('Dahlia','9056'),('Lara','9681'),('Ali','3152'),('Iona','3652'),('Clayton','1429'),('Fulton','1588'),('Blair','4301'),('Molly','2595'),('Davis','6345');
 INSERT INTO FoodItemCategories(categoryId,name) VALUES(1,'Chicken'),(2,'Fish'),(3,'Rice'),(4,'Noodle'),(5,'Drinks');
+INSERT INTO FoodItems(fooditemid, foodname, price, daily_limit, itemavailability, rid, category) VALUES(1, "ice cream", 1, 100, 20, "T", 1, 1);
